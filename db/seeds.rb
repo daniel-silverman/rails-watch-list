cd6ff3931050e4b65a8db52f7d88ebea6450a521
Daniel Silverman <daniel.aa.silverman@gmail.com>
2021-05-13 18:02:35 -0400

    Backup files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+100.times do
+  Movie.create(
+    title: Faker::Movie.title,
+    overview: Faker::Fantasy::Tolkien.poem,
+    poster_url: 'https://source.unsplash.com/random/400x600',
+    rating: rand(1..9)
+  )
+end
